6b76bb81e38b260b2822a408b222be675ea99ce2
Héctor <hsoriano9986@gmail.com>
2024-03-10 12:16:02 +0100

    react inicializado, definida estructura basica  en app.jsx, falta instalar dependencias

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import { Body } from './pages/Body/Body';
+
+function App() {
+
+
+  return (
+    <>
+      <Header ></Header>
+      <Body ></Body>
+      <Footer ></Footer>
+    </>
+  )
+}
+
+export default App
