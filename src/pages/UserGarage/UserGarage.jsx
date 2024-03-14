cb595bcb48c863cd96485deda811a79b86b3c7bc
Héctor <hsoriano9986@gmail.com>
2024-03-14 01:38:34 +0100

    actualización estilos css para vista garage, botones de paginación y tarjeta de coches de usuario

--- src/pages/UserGarage/UserGarage.jsx
+++ src/pages/UserGarage/UserGarage.jsx
@@ -128,6 +128,14 @@ export const UserGarage = () => {
     <div className="profileDetails">
         {garageEditable === true ? (
           <Card className="garageEdit" id="garageEdit">
+            <div className="pageDiv">
+            <Button className="pageButton" variant="secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-left" viewBox="0 0 16 16">
+            <path d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753"/>
+            </svg>
+            </Button>
+            </div>
+            <div className="carCard">
             <div className="imageWrapper">
             {profileData.user?.car[0]?.car_image ? (
             <Card.Img
@@ -153,6 +161,14 @@ export const UserGarage = () => {
             <p className="carSpecButtonTitle" >Car Specs</p>
             </Button>
             </CardBody>
+            </div>
+            </div>
+            <div className="pageDiv">
+            <Button className="pageButton" variant="secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
+            <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
+            </svg>
+            </Button>
             </div>
           </Card>
         ):null}
